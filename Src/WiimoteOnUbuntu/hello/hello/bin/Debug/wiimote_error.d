f3c1fdb837afe81ea317a5c7296dd29a7647c5e4
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-06-26 23:01:41 +0200

    Move projects

--- Src/WiimoteOnUbuntu/hello/hello/bin/Debug/wiimote_error.d
+++
@@ -1,4 +0,0 @@
-/home/mike/Projects/hello/hello/bin/Debug/wiimote_error.o: \
- /home/mike/Projects/hello/hello/wiimote_error.c \
- /home/mike/Projects/hello/hello/wiimote.h \
- /home/mike/Projects/hello/hello/wiimote_error.h
--- Src/WiimoteOnUbuntu/hello/hello/bin/Debug/wiimote_ir.d
+++
@@ -1,5 +0,0 @@
-/home/mike/Projects/hello/hello/bin/Debug/wiimote_ir.o: \
- /home/mike/Projects/hello/hello/wiimote_ir.c \
- /home/mike/Projects/hello/hello/wiimote.h \
- /home/mike/Projects/hello/hello/wiimote_io.h \
- /home/mike/Projects/hello/hello/wiimote_report.h
